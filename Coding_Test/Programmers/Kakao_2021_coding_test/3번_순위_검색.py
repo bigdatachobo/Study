5a7b96e480c390f3e5d8b0d43638c118a7c4ee2e
bigdatachobo <61238033+bigdatachobo@users.noreply.github.com>
2021-09-17 19:56:24 +0900

    Create 3번_순위_검색.py

---
+++ Coding_Test/Programmers/Kakao_2021_coding_test/3번_순위_검색.py
@@ -0,0 +1,35 @@
+# https://programmers.co.kr/learn/courses/30/lessons/72412
+from functools import reduce
+from collections import defaultdict
+from bisect import insort, bisect_left
+
+def solution(info, query):
+    table = {"c": 3, "j": 5, "p": 6, "b": 6, "f": 5, "s": 6, "-": 0}
+    conv = lambda l, t: (reduce(lambda a, k: (a << 3) + t(table[k[0]]), l[:-1], 0), int(l[-1]))
+    info = list(map(lambda s: conv(s.split(" "), lambda x: 7 - x), info))
+    query = list(map(lambda s: conv([c for c in s.split(" ") if c != "and"], lambda x: x), query))
+    d = defaultdict(list)
+    for k, v in info:
+        insort(d[k], v)
+    return [sum([len(l) - bisect_left(l, v) for k, l in d.items() if not k & q]) for q, v in query]
+  
+  '''
+  info
+  ["java backend junior pizza 150",
+  "python frontend senior chicken 210",
+  "python frontend senior chicken 150",
+  "cpp backend senior pizza 260",
+  "java backend junior chicken 80",
+  "python backend senior chicken 50"]
+  
+  query
+  ["java and backend and junior and pizza 100",
+  "python and frontend and senior and chicken 200",
+  "cpp and - and senior and pizza 250",
+  "- and backend and senior and - 150",
+  "- and - and - and chicken 100",
+  "- and - and - and - 150"]
+  
+  result
+  [1,1,1,1,2,4]
+  '''
